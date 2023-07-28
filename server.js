d5533adeb2443841553f90376bfb4284e1c772b0
Harpreet Singh <78143203+singhWeb@users.noreply.github.com>
2023-07-28 08:11:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,39 @@
 const express = require("express");
-const axios = require("axios");
-
+const axios = require("axios"); // Add this line to import axios
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Replace this URL with your Google Apps Script URL
 const googleAppsScriptUrl =
-    "https://script.google.com/macros/s/AKfycbxT-SGKuFZm5BhUZeMJmwraTGvnLIIHZKWzPUqgcWLA281XzqPfilGBsbBD0kpA6tXB/exec";
+  "https://script.google.com/macros/s/AKfycbxT-SGKuFZm5BhUZeMJmwraTGvnLIIHZKWzPUqgcWLA281XzqPfilGBsbBD0kpA6tXB/exec";
 
-// Endpoint to receive data from your device
+// Handle the "/send-data" route to receive data from your device
 app.get("/send-data", async (req, res) => {
-  console.log("Received data:", req.query);
-    try {
-        // Extract data from the query parameters (latitude, longitude, weight)
-        const { latitude, longitude, weight } = req.query;
+  try {
+    // Extract data from the query parameters (latitude, longitude, weight)
+    const { latitude, longitude, weight } = req.query;
 
-        // Make an HTTP GET request to the Google Apps Script URL with the received data
-        const response = await axios.get(googleAppsScriptUrl, {
-            params: {
-                latitude,
-                longitude,
-                weight,
-            },
-        });
+    // Debugging: Log the received data to the console
+    console.log("Received data:", { latitude, longitude, weight });
 
-        // If the Google Apps Script processed the data successfully, respond to the device
-        if (response.data === "SUCCESS") {
-            res.send("Data received and processed successfully!");
-        } else {
-            res.send("Failed to process the data.");
-        }
-    } catch (error) {
-        console.error("Error processing data:", error.message);
-        res.status(500).send("Error processing data.");
-    }
+    // Process the received data as needed (e.g., send it to Google Apps Script)
+    // ... 
+    const response = await axios.get(googleAppsScriptUrl, {
+      params: {
+        latitude,
+        longitude,
+        weight,
+      },
+    });
+
+    // If the Google Apps Script processed the data successfully, respond to the device
+    res.send("Data received and processed successfully!");
+  } catch (error) {
+    console.error("Error processing data:", error.message);
+    // If there's an error while processing the data, respond to the device with an error message
+    res.status(500).send("Failed to process the data.");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
